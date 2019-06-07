ba31d640b28f2052830d4ed542cc63cbae697aa9
Kent Tokunaga <KentTokunaga@gmail.com>
2019-06-07 11:17:22 -0500

    Add comments

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
+# Use Azure Resource Manager as provider
 provider "azurerm" {
   version = "=1.30.1"
 }
 
+# Create resource group
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "eastus"
