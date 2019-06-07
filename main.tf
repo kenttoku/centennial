84b48dd8f16deae3f294354b6efbffde17f029cf
Kent Tokunaga <KentTokunaga@gmail.com>
2019-06-07 11:26:43 -0500

    Create subnet

--- main.tf
+++ main.tf
@@ -12,3 +12,19 @@ resource "azurerm_resource_group" "rg" {
     environment = "TF sandbox"
   }
 }
+
+# Create virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+}
+
+# Create subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "myTFSubnet"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  address_prefix       = "10.0.1.0/24"
+}
