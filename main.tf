cc5f14182a5b213e484b0266cd797cf931390629
Kent Tokunaga <KentTokunaga@gmail.com>
2019-06-07 11:29:52 -0500

    Create a Linux virtual machine

--- main.tf
+++ main.tf
@@ -70,3 +70,36 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = "${azurerm_public_ip.publicip.id}"
   }
 }
+
+# Create a Linux virtual machine
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "myTFVM"
+  location              = "eastus"
+  resource_group_name   = "${azurerm_resource_group.rg.name}"
+  network_interface_ids = ["${azurerm_network_interface.nic.id}"]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_os_disk {
+    name              = "myOsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04.0-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "myTFVM"
+    admin_username = "plankton"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
