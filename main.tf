297642e65b69ec3c83159dbb3c5ec5ef9a9b07d1
Kent Tokunaga <KentTokunaga@gmail.com>
2019-06-15 23:17:13 -0500

    Create virtual network with hardcoded values

--- main.tf
+++ main.tf
@@ -8,3 +8,13 @@ resource "azurerm_resource_group" "main" {
   name     = "${var.main_resource_group["name"]}"
   location = "${var.location}"
 }
+
+module "network" "main" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  location            = "${var.location}"
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.0.0/24"]
+  subnet_names        = ["subnet1"]
+}
+
