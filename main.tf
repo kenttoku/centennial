2e422b2a08e01318dbb9442a2da37bd0a390bff7
Kent Tokunaga <KentTokunaga@gmail.com>
2019-06-16 19:09:35 -0500

    Create without module

--- main.tf
+++ main.tf
@@ -9,9 +9,16 @@ resource "azurerm_resource_group" "main" {
   location = "${var.location}"
 }
 
-module "network" {
-  source              = "Azure/network/azurerm"
-  vnet_name           = "${var.prefix}-vnet"
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.main.location}"
   resource_group_name = "${azurerm_resource_group.main.name}"
-  location            = "${var.location}"
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "subnet"
+  resource_group_name  = "${azurerm_resource_group.main.name}"
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  address_prefix       = "10.0.0.0/24"
 }
--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
 output "main_resource_group_name" {
   value = "${azurerm_resource_group.main.name}"
 }
-
-output "main_vnet_subnets" {
-  value = "${module.network.vnet_subnets}"
-}
