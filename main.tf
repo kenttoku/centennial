d1dbb2833a1fd9310a54e798153e52240e0bc02a
Kent Tokunaga <KentTokunaga@gmail.com>
2019-06-16 19:14:03 -0500

    Add public ip

--- main.tf
+++ main.tf
@@ -17,8 +17,15 @@ resource "azurerm_virtual_network" "main" {
 }
 
 resource "azurerm_subnet" "main" {
-  name                 = "subnet"
+  name                 = "${var.prefix}-subnet"
   resource_group_name  = "${azurerm_resource_group.main.name}"
   virtual_network_name = "${azurerm_virtual_network.main.name}"
   address_prefix       = "10.0.0.0/24"
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-public-ip"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  allocation_method   = "Static"
 }
